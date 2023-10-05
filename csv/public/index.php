ab544fc712cdfe30925bf46dcf7c93b8b3e8e3f1
Chris <trehand.christian@gmail.com>
2023-10-05 12:19:14 +0200

    init

--- csv/public/index.php
+++ csv/public/index.php
@@ -1,2 +1,15 @@
-<?php 
-echo "ok";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css/style.css" rel="stylesheet">
+    
+</head>
+<body>
+<div id="drop_zone">dfhfghf
+    dfgdg<br>
+</div>
+</body>
+</html><?php 
